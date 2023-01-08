5e65d7008fb7df59410e7ddd8e5231db341919e2
Prince Kanaugia <pkanaugia@gmail.com>
2023-01-08 15:45:06 +0530

    completed api fetching and output process

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var translateInput = document.querySelector("#translate-input")
 var translateOutput = document.querySelector("#translate-output");
+var minionApiServerUrl = "https://api.funtranslations.com/translate/minion.json";
 
 
+/* input-output processing Example
 function clickTranslateHandler() {
     translateOutput.innerText = "input : "+translateInput.value;
+};*/
+
+function getTranslationUrl(text) {
+    var translateUrl = minionApiServerUrl + "?" + "text=" + text;
+    return translateUrl;
+
+};
+
+function errorHandler(error){
+    alert("error occured!! on serverside. please try again later");
+
+};
+
+function clickTranslateHandler() {
+    var inputText = translateInput.ariaValueMax; // taking input
+
+    fetch(getTranslationUrl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        translateOutput.innerText = translatedText;
+    })
+    .catch(errorHandler) // we don't have to pass error argument in errorHandler(), it catches the error that occured and pass it.
+
 };
 
 btnTranslate.addEventListener("click", clickTranslateHandler)
